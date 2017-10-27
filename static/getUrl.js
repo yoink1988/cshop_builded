c8e1e5a1dc4e406cfe2d75f09446870cd7d66ea9
Ilya Erinchak <yoink1988@gmail.com>
2017-10-27 13:25:40 +0300

    cshop_builded

---
+++ static/getUrl.js
@@ -0,0 +1,14 @@
+
+
+var urlHome = 'http://localhost/carShop/client/api/'
+var urlClass = 'http://192.168.0.15/~user9/carShop/client/api/'
+
+function getUrl(){
+  return urlClass
+}
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+
